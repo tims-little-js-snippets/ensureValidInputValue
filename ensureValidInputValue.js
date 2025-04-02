5b9309e849b22b8f55dfda2564628ed2bab51d4b
Tim M <82600217+tmetcalfe89@users.noreply.github.com>
2025-04-02 03:32:06 +0000

    formatting

--- ensureValidInputValue.js
+++ ensureValidInputValue.js
@@ -1,22 +1,28 @@
 const ensureValidInputValue = (() => {
   const cache = new Map();
 
-  function track(targeter, matcher, {
-    parent = document
-  } = {}) {
-    if (typeof targeter != "string") throw new Error(`Targeter must be instance of string: ${targeter}`);
-    if (!(matcher instanceof RegExp)) throw new Error(`Matcher must be instance of RegExp: ${matcher}`);
+  function track(targeter, matcher, { parent = document } = {}) {
+    if (typeof targeter != "string")
+      throw new Error(`Targeter must be instance of string: ${targeter}`);
+    if (!(matcher instanceof RegExp))
+      throw new Error(`Matcher must be instance of RegExp: ${matcher}`);
     const inputs = parent.querySelectorAll(targeter);
-    inputs.forEach(element => {
-      if (!(element instanceof HTMLInputElement)) throw new Error(`Invalid selector result (not input): ${element}`);
-      cache.set(element, element.value);
-      element.addEventListener("input", () => {
+    inputs.forEach((element) => {
+      if (!(element instanceof HTMLInputElement))
+        throw new Error(`Invalid selector result (not input): ${element}`);
+      const inputHandler = () => {
+        const cached = cache.get(element);
         if (!matcher.test(element.value)) {
-          element.value = cache.get(element);
+          element.value = cached.value;
         } else {
-          cache.set(element, element.value);
+          cache.set(element, { ...cached, value: element.value });
         }
-      })
+      };
+      cache.set(element, {
+        value: element.value,
+        handler: inputHandler,
+      });
+      element.addEventListener("input", inputHandler);
     });
   }
 
